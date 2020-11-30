45516fffce5511df3dc372a34df6d9c64d0aac24
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-11-29 22:25:25 -0500

    get x, get y, and distanceTo constructed

--- Point.java
+++ Point.java
@@ -1,6 +1,6 @@
 public class Point{
   private double x,y;
-  
+
   public Point(double X, double Y) {
     x = X;
     y = Y;
@@ -9,5 +9,17 @@ public class Point{
   public Point(Point p) {
     x = p.x;
     y = p.y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public double distanceTo(Point other) {
+    return (Math.sqrt(Math.pow(x-other.x,2) + Math.pow(y-other.y, 2)));
   }
 }
